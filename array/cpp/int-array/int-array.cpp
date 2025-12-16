4cb34909c002dbaebee91c6fb1c9c4a4d9f30e4b
Eric Hebert <ehebert@ucsd.edu>
2025-12-16 11:23:46 -0800

    Finished writing functions

--- array/cpp/int-array/int-array.cpp
+++ array/cpp/int-array/int-array.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 IntArray::IntArray(int initialSize) {
-    if(initialSize <= 0) {
+    if(initialSize <= 0 || initialSize >= 101) {
         initialSize = 2;
     }
     capacity = initialSize;
@@ -44,6 +44,9 @@ void IntArray::prependElement(int value) {
 }
 
 void IntArray::insertAt(int value, int index) {
+    if(index >= numOfElements || index < 0) {
+        throw std::out_of_range("IntArray::at() index out of range");
+    }
     if(numOfElements == capacity) {
         expandCapacity();
     }
@@ -54,6 +57,42 @@ void IntArray::insertAt(int value, int index) {
     ++numOfElements;
 }
 
+bool IntArray::findElement(int value) const {
+    if(numOfElements == 0) {
+        return false;
+    }
+    for(int i = 0; i <= numOfElements; ++i) {
+        if(data[i] == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int& IntArray::at(unsigned int index) {
+    if(index >= numOfElements) {
+        throw std::out_of_range("IntArray::at() index out of range");
+    }
+    return *(data+index);
+}
+
+const int& IntArray::at(unsigned int index) const {
+    if(index >= numOfElements) {
+        throw std::out_of_range("IntArray::at() index out of range");
+    }
+    return *(data+index);
+}
+
+bool IntArray::isEmpty() const {
+    return numOfElements == 0;
+}
+
+void IntArray::clear() {
+    for(int i = 0; i <= numOfElements; ++i) {
+        data[i] = NULL;
+    }
+}
+
 unsigned int IntArray::getCapacity() {
     return capacity;
 }
@@ -62,8 +101,14 @@ unsigned int IntArray::size() const {
     return numOfElements;
 }
 
-bool IntArray::isEmpty() const {
-    return numOfElements == 0;
+void IntArray::ShrinkToFit() {
+    int* newData = new int[numOfElements+1];
+    for(int i = 0 ; i <= numOfElements; ++i) {
+        newData[i] = data[i];
+    }
+    delete[] data;
+    data = newData;
+    capacity = numOfElements+1;
 }
 
 void IntArray::printList() {
@@ -75,6 +120,31 @@ void IntArray::printList() {
         }
     }
     cout << "}" << endl;
+}
+
+IntArray& IntArray::operator=(const IntArray& other) {
+    if (this == &other) {
+        return *this;
+    }
+    delete[] data;
+    numOfElements = other.numOfElements;
+    data = new int[numOfElements];
+    for (unsigned int i = 0; i < numOfElements; ++i) {
+        data[i] = other.data[i];
+    }
+    return *this;
+}
+
+IntArray& IntArray::operator=(IntArray&& other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+    delete[] data;
+    data = other.data;
+    numOfElements = other.numOfElements;
+    other.data = nullptr;
+    other.numOfElements = 0;
+    return *this;
 }
 
 bool IntArray::operator==(const IntArray& otherArr) const {
@@ -89,3 +159,14 @@ bool IntArray::operator==(const IntArray& otherArr) const {
     return true;
 }
 
+bool IntArray::operator!=(const IntArray& otherArr) const {
+    return !(*this == otherArr);
+}
+
+int& IntArray::operator[](unsigned int index) {
+    return at(index);
+}
+
+const int& IntArray::operator[](unsigned int index) const {
+    return at(index);
+}
